6d062898835326f67474c015bb64a708783f6bc3
Wesley Dunlap <wtdunlap@gmail.com>
2023-12-11 13:48:48 -0500

    finalized and commented code more better good

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,8 @@ const specialCharArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
 
 
 function generatePassword() {
-
+  // Resets variables for clean starts
+  // =================================
   // reset variable to stock between runs
   length = 0;
   isLower = false;
@@ -62,12 +63,19 @@ function generatePassword() {
   while (length < 8 || length > 128)
 
   // Prompts for configuration
+  // =========================
+  // Prompts for upper lower numeric and special characters
+
   isLower = window.prompt("Does this need to be lower case?", "Yea or cancel for nah");
   isUpper = window.prompt("How about upper case?", "Yea or cancel for nah");
   isNumeric = window.prompt("What about numbers?", "Yea or cancel for nah");
   isSpecial = window.prompt("And those special guys?", "Yea or cancel for nah");
 
+
+  // Building password list
+  // ======================
   // Pull random char from lowerAlpha
+
   if (isLower) {
     for (var i = 0; i < length; i++) {
       placeholder += lowerAlpha.charAt(Math.floor(Math.random() * lowerAlpha.length));
@@ -95,10 +103,69 @@ function generatePassword() {
     }
   }
 
+  // Gather all characters and select password characters
+  // ====================================================
   // Pull random char from placeholder for pending password
+
   for (i = 0; i < length; i++) {
     pending += placeholder.charAt(Math.floor(Math.random() * placeholder.length));
   }
+  
+  // Double check inclusion of all required character types
+  // ======================================================
+  // if character at pending i index is not found in string lowerAlpha
+  // replace character 0 with random character in lowerAlpha
+  
+  if (isLower) {
+    for (i = 0; i < pending.length; i++) {
+      var index = lowerAlphaArray.indexOf(lowerAlphaArray, pending[i]);
+      if (index <= -1) {
+        pending[0] = lowerAlpha.charAt(Math.floor(Math.random() * lowerAlpha.length));
+      }   
+    }
+  }
+  
+  // if character at pending i index is not found in string upperAlpha
+  // replace character 1 with random character in upperAlpha
+  
+  if (isUpper) {
+    for (i = 0; i < pending.length; i++) {
+      index = upperAlphaArray.indexOf(upperAlphaArray, pending[i]);
+      if (index <= -1) {
+        pending[1] = lowerAlpha.charAt(Math.floor(Math.random() * lowerAlpha.length));
+      }   
+    }
+  }
+  
+  // if character at pending i index is not found in string numericChar
+  // replace character 2 with random character in numericChar
+  
+  if (isNumeric) {
+    for (i = 0; i < pending.length; i++) {
+      index = numericCharArray.indexOf(numericCharArray, pending[i]);
+      if (index <= -1) {
+        pending[2] = numericChar.charAt(Math.floor(Math.random() * numericChar.length)); 
+      }   
+    }
+  }
+  
+  // if character at pending i index is not found in string specialChar
+  // replace character 3 with random character in specialChar
+  
+  if (isSpecial) {
+    for (i = 0; i < pending.length; i++) {
+      index = specialCharArray.indexOf(specialCharArray, pending[i]);
+      if (index <= -1 ) {
+        pending[3] = specialChar.charAt(Math.floor(Math.random() * specialChar.length));
+      }   
+    }
+  }
+  
+  // Final result
+  // ============
+  // returns final result
+
+  return pending;  
 }
 
 
